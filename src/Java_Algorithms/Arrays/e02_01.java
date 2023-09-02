e16de792047a8fde6af3fcf303128b49172a2276
Piotr <xmestre@wp.pl>
2023-09-02 07:31:21 +0100

    Java Algorithms - array algorithms

--- src/Java_Algorithms/Arrays/e02_01.java
+++ src/Java_Algorithms/Arrays/e02_01.java
@@ -9,6 +9,9 @@ public class e02_01 {
         int[] arr = {1, 2, 3, 4, 5, 6};
         System.out.println(linearSearch(arr, 9));
         System.out.println(linearSearch(arr, 4));
+
+        System.out.println(linearSearch3(arr, 9));
+        System.out.println(linearSearch3(arr, 4));
     }
 
     public static boolean linearSearch(int[] arr, int item){
@@ -19,5 +22,26 @@ public class e02_01 {
             }
         }
         return false;
+    }
+    // second option instead boolean return the number
+    public static int linearSearch2(int[] arr, int item){
+        for(int current : arr){
+            if(current == item){
+                return item;
+            }
+        }
+
+        return -1;
+    }
+
+    // third option instead boolean have an option of returning null
+    public static Integer linearSearch3(int[] arr, int item){
+        for(int current : arr){
+            if(current == item){
+                return item;
+            }
+        }
+
+        return null;
     }
 }
