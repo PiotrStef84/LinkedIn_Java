7bef76c5914d16c1878af8bc96cdd3ebffb45d06
Piotr <xmestre@wp.pl>
2023-09-04 10:31:38 +0100

    Java Algorithms - linked-list algorithms, creating custom linked list structure

---
+++ src/Java_Algorithms/LinkedLists/CustomLinkedList.java
@@ -0,0 +1,15 @@
+package Java_Algorithms.LinkedLists;
+
+public class CustomLinkedList {
+
+    Node head;
+
+    public void displayContents(){
+        Node current = head;
+        while(current != null){
+            System.out.print(current.data + "->");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
---
+++ src/Java_Algorithms/LinkedLists/Node.java
@@ -0,0 +1,10 @@
+package Java_Algorithms.LinkedLists;
+
+public class Node {
+    public int data;
+    public Node next;
+
+    public Node(int d){
+        this.data = d;
+    }
+}
