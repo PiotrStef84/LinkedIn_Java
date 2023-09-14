0b7452a5b2bad44ea838547ec073c2d28c738444
Piotr <xmestre@wp.pl>
2023-09-14 18:24:55 +0100

    Java: Data Structures - CollectionsExercise start, plus adding test class in JUnit 5

---
+++ src/Java_Data_Structures/CollectionsExercise.java
@@ -0,0 +1,29 @@
+package Java_Data_Structures;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Set;
+
+public class CollectionsExercise {
+
+    void addToEndOfLinkedList(LinkedList<Integer> numbers, int i) {
+        numbers.add(i);
+    }
+
+    void addToStartOfLinkedList(LinkedList<Integer> numbers, int i) {
+
+    }
+
+    void removeItemFromTopOfStack(Deque<Integer> stack) {
+
+    }
+
+    void removeItemFromFrontOfQueue(Queue<Integer> queue) {
+
+    }
+
+    void addItemToTreeSet(Set<String> set, String item) {
+
+    }
+}
