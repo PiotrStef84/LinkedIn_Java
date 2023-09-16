b0226737e130261a17c7dc1e076167ae759d8ce9
Piotr <xmestre@wp.pl>
2023-09-16 18:09:28 +0100

    Learning Java Collections - Collections Framework

--- src/Learning_Java_Collections/Java_Collections_Framework/e01_04.java
+++ src/Learning_Java_Collections/Java_Collections_Framework/e01_04.java
@@ -1,18 +1,27 @@
 package Learning_Java_Collections.Java_Collections_Framework;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class e01_04 {
 
     public static void main(String[] args) {
 
         Room cambridge = new Room("Cambridge", "Premiere Room", 4, 175.00);
         Room manchester = new Room("Manchester", "Suite", 5, 250.0);
+        Room flabbergasted = new Room("Flabbergasted", "Guest Room", 3, 125.0);
 
-        double total = getPotentialRevenue(cambridge, manchester);
+        Collection<Room> rooms = List.of(cambridge, manchester, flabbergasted);
+
+        double total = getPotentialRevenue(rooms);
         System.out.println(total);
     }
 
-    private static double getPotentialRevenue(Room room1, Room room2) {
-        return room1.getRate() + room2.getRate();
+    private static double getPotentialRevenue(Collection<Room> rooms) {
+        return rooms.stream()
+                .mapToDouble(r -> r.getRate())
+                .sum();
     }
 }
 
