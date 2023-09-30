4779f3de59009550bfc4f9f0776043c5395035d8
Piotr <xmestre@wp.pl>
2023-09-30 15:40:33 +0100

    Learning Java Collections - Challenge adding and removing collection elements

--- src/Learning_Java_Collections/The_Foundations_of_Java_Collections/RoomService.java
+++ src/Learning_Java_Collections/The_Foundations_of_Java_Collections/RoomService.java
@@ -1,14 +1,17 @@
 package Learning_Java_Collections.The_Foundations_of_Java_Collections;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class RoomService {
 
 	// 1. Declare a Collection to store Room Inventory
+	Collection<Room> c;
 
 	public RoomService() {
 		
 	// 2. Initialize Collection and assign it to the Room Inventory
+	c = new ArrayList<Room>();
 
 	}
 
@@ -16,12 +19,13 @@ public class RoomService {
 		
 	// 3. Return the Room Inventory
 		
-		return null;
+	return c;
 	}
 	
 	public void createRoom(String name, String type, int capacity, double rate) {
 	
 	// 4. Add a new Room to the Room Inventory using the provided parameters
+	c.add(new Room (name, type, capacity, rate));
 
 	}
 
@@ -29,11 +33,16 @@ public class RoomService {
 	
 	// 5. Add the Rooms provided in the Array to the Room Inventory
 
+		for(int i= 0; i< rooms.length; i++){
+			c.add(rooms[i]);
+		}
+
 	}
 	
 	public void removeRoom(Room room) {
 		
 	// 6. Remove the provided Room from the Room Inventory
+	c.remove(room);
 
 	}
 
