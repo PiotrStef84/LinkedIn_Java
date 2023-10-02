c77c008aa0f4f37eda39210d9ee1a2f71493558c
Piotr <xmestre@wp.pl>
2023-10-02 22:32:15 +0100

    Learning Java Collections - Iterating Collections.

--- src/Learning_Java_Collections/Chapter_3_Iterating_Collections/e03_02.java
+++ src/Learning_Java_Collections/Chapter_3_Iterating_Collections/e03_02.java
@@ -3,6 +3,7 @@ package Learning_Java_Collections.Chapter_3_Iterating_Collections;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Iterator;
 
 public class e03_02 {
 
@@ -16,6 +17,19 @@ public class e03_02 {
         Room westminister = new Room("Westminister", "Premiere Room", 4, 200.00);
 
         Collection<Room> rooms = new ArrayList<>(Arrays.asList(piccadilly, cambridge, westminister));
+
+        // Popular approach prior to Java 5 -> using an iterator with the while loop
+        Iterator<Room> iterator = rooms.iterator();
+
+        while(iterator.hasNext()){
+            Room room = iterator.next();
+            System.out.println(room.getName());
+        }
+
+        // using an enhanced for each loop
+        for (Room room : rooms) {
+            System.out.println(room.getName());
+        }
 
     }
 }
