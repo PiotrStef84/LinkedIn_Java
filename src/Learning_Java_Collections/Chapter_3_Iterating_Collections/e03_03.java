ab9530b08214ad20a6f5a3b3bb5d6d35ce5ed459
Piotr <xmestre@wp.pl>
2023-10-06 17:29:11 +0100

    Learning Java Collections - Modifying while iterating collections.

--- src/Learning_Java_Collections/Chapter_3_Iterating_Collections/e03_03.java
+++ src/Learning_Java_Collections/Chapter_3_Iterating_Collections/e03_03.java
@@ -3,6 +3,7 @@ package Learning_Java_Collections.Chapter_3_Iterating_Collections;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Iterator;
 
 public class e03_03 {
 
@@ -22,15 +23,42 @@ public class e03_03 {
 
         // This code will cause ConcurrentModificationException - an exception that is thrown when modifying
         // collection while iterating through it.
+//        for(Room room : rooms) {
+//
+//            if(room.isPetFriendly()) {
+//                rooms.remove(room);
+//            }
+//        }
+
+        // To avoid the error we create a second collection, gather all the rooms that are to be removed
+        // and remove them after finishing iterating
+        Collection<Room> removesRooms = new ArrayList<>();
         for(Room room : rooms) {
 
+            if(!room.isPetFriendly()) {
+                removesRooms.add(room);
+            }
+        }
+
+        // remove all gathered rooms by using the bulk operator
+        rooms.removeAll(removesRooms);
+
+        // Another way of removing element of the collection
+        Iterator<Room> iterator = rooms.iterator();
+        while(iterator.hasNext()){
+            Room room = iterator.next();
+
             if(room.isPetFriendly()) {
-                rooms.remove(room);
+                iterator.remove();
             }
 
         }
 
-        System.out.println(rooms);
+        //System.out.println(rooms);
+
+        rooms.stream().forEach(room -> {
+            System.out.println(room.getName());
+        });
 
     }
 }
