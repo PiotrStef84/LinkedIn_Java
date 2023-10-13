bba186d627abdee115af43a6ab91522854ec8511
Piotr <xmestre@wp.pl>
2023-10-13 19:00:52 +0100

    Learning Java Collections - HashSet

---
+++ src/Learning_Java_Collections/Chapter_4_Set/e04_02.java
@@ -0,0 +1,17 @@
+package Learning_Java_Collections.Chapter_4_Set;
+
+public class e04_02 {
+
+    public static void main(String[] args) {
+
+        //
+        // 4.2. HashSet
+
+        Room piccadilly = new Room("Piccadilly", "Guest Room", 3, 125.00);
+        Room oxford = new Room("Oxford", "Suite", 5, 225.0);
+        Room oxfordDuplicate = new Room("Oxford", "Suite", 5, 225.0);
+        Room victoria = new Room("Victoria", "Suite", 5, 225.00);
+
+
+    }
+}
