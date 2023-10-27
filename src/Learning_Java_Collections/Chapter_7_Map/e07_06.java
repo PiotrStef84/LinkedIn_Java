687ef6e18bfdb63b676bfede1b3a38529a27efb6
Piotr <xmestre@wp.pl>
2023-10-27 17:44:46 +0100

    Learning Java Collections - Iterating Maps

--- src/Learning_Java_Collections/Chapter_7_Map/e07_06.java
+++ src/Learning_Java_Collections/Chapter_7_Map/e07_06.java
@@ -2,6 +2,8 @@ package Learning_Java_Collections.Chapter_7_Map;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 public class e07_06 {
 
@@ -20,6 +22,19 @@ public class e07_06 {
         Map<Room, Guest> assignments = new HashMap<>();
         assignments.put(oxford, maria);
         assignments.put(piccadilly, john);
+
+        // To iterate through the map we are getting an entry set
+        // this is in fact a set and can be iterated through just like other collections
+        // through enhanced for loop or streams
+
+        // assigning the result of entrySet() method to a set
+        //Set<Entry<Room, Guest>> cv =  assignments.entrySet();
+
+        for(Entry<Room, Guest> entry : assignments.entrySet()){
+            Room r = entry.getKey();
+            Guest g = entry.getValue();
+            System.out.printf("%s : %s%n" , r.getName(), g.getFirstName());
+        }
 
 
 
