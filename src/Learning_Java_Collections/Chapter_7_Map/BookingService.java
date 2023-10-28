199cd7f5e7514df241b90b9702a15ca1648185be
Piotr <xmestre@wp.pl>
2023-10-28 09:59:18 +0100

    Learning Java Collections - Challenge - working with maps ... completed

--- src/Learning_Java_Collections/Chapter_7_Map/BookingService.java
+++ src/Learning_Java_Collections/Chapter_7_Map/BookingService.java
@@ -2,6 +2,7 @@ package Learning_Java_Collections.Chapter_7_Map;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class BookingService {
 
@@ -17,6 +18,18 @@ public class BookingService {
 		 * Returns a boolean that indicates if the Guest was
 		 * successfully placed in the room.
 		 */
+
+//		-------------- Solution from course ------------------
+//		return this.bookings.putIfAbsent(room, guest) == null;
+//		------------------------------------------------------
+
+		// Book if Absent will return null if the room was empty
+		Guest guest2 = bookings.putIfAbsent(room, guest);
+
+		// if the room was empty guest2 is null, that indicates that booking was successful
+		if(guest2 == null){
+			return true;
+		}
 		
 		return false;
 	}
@@ -27,7 +40,30 @@ public class BookingService {
 		 * 2. Returns a double that totals the rate of each Room booked
 		 * in the bookings Map.
 		 */
-		return 0;
+
+
+//		-------------- Solution from course ------------------
+//		return this.bookings.keySet().stream()
+//				.mapToDouble(Room::getRate)
+//				.sum();
+//		------------------------------------------------------
+
+
+//      ------------------- MY FIRST SOLUTION --------------------
+//		double totalRevenue = 0;
+//		for (Map.Entry<Room, Guest> entry: bookings.entrySet()) {
+//			totalRevenue = totalRevenue + entry.getKey().getRate();
+//		}
+//		return totalRevenue;
+//      ----------------------------------------------------------
+
+//      -------------------- MY SECOND SOLUTION --------------------------
+		// The same thing but done using streams
+		Set<Map.Entry<Room, Guest>> entries = bookings.entrySet();
+		double totalRevenue = entries.stream().mapToDouble(entry -> entry.getKey().getRate()).sum();
+
+		return totalRevenue;
+//      -------------------------------------------------------------------
 	}
 	
 	public Map<Room, Guest> getBookings() {
