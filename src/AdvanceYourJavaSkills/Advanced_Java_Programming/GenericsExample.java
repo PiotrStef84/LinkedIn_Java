88ab42540161043f04b7f8192678bdca0b0c883b
Piotr <xmestre@wp.pl>
2023-11-09 21:45:37 +0000

    Advanced Java Programming - intro to Generics

--- src/AdvanceYourJavaSkills/Advanced_Java_Programming/GenericsExample.java
+++ src/AdvanceYourJavaSkills/Advanced_Java_Programming/GenericsExample.java
@@ -12,10 +12,11 @@ public class GenericsExample {
         names.add("Kelly");
         String name = (String) names.get(0);
         System.out.println("First name: " + name);
-        //names.add(7);
+        names.add(7); // ... allows for adding numbers
 
 
         // Example with Generics
+        // This code is safer and no casting is needed.
         List<String> names2 = new ArrayList();
         names2.add("Kelly");
         String name2 = names2.get(0);
