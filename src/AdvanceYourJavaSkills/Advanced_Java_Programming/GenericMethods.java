cf6b2649889d61d0dbac8a4d0cd92ff1810bd782
Piotr <xmestre@wp.pl>
2023-11-13 18:23:25 +0000

    Advanced Java Programming - Generics Methods

--- src/AdvanceYourJavaSkills/Advanced_Java_Programming/GenericMethods.java
+++ src/AdvanceYourJavaSkills/Advanced_Java_Programming/GenericMethods.java
@@ -11,9 +11,18 @@ public class GenericMethods {
     static Integer[] intArray = {1, 2, 3, 4, 5};
     static Boolean[] boolArray = {true, false, true};
 
+
     // iterates through objects in an array and adds them to the list
-    public static List arrayToList(Object[] array, List<Object> list) {
-        for (Object object : array) {
+//    public static List arrayToList(Object[] array, List<Object> list) {
+//        for (Object object : array) {
+//            list.add(object);
+//        }
+//        return list;
+//    }
+
+    // Introducing generics to keep the code flexible, and being able to discover potential errors in the code
+    public static <T> List<T> arrayToList(T [] array, List<T> list) {
+        for (T object : array) {
             list.add(object);
         }
         return list;
