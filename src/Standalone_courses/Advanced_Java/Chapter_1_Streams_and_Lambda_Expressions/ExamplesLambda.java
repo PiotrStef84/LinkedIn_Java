30c6742f300cd6e923591de034cd4e5dab4f8b15
Piotr <xmestre@wp.pl>
2023-12-08 13:22:44 +0000

    Advanced Java - Lambda Expressions

--- src/Standalone_courses/Advanced_Java/Chapter_1_Streams_and_Lambda_Expressions/ExamplesLambda.java
+++ src/Standalone_courses/Advanced_Java/Chapter_1_Streams_and_Lambda_Expressions/ExamplesLambda.java
@@ -1,5 +1,6 @@
 package Standalone_courses.Advanced_Java.Chapter_1_Streams_and_Lambda_Expressions;
 
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 public class ExamplesLambda {
@@ -8,9 +9,21 @@ public class ExamplesLambda {
     // 1.2. Lambda Expressions
 
     public static void main(String[] args) {
+
+        // Predicate -> built in functional interface that returns a boolean
         Predicate<String> largerThan5 = s -> s.length() > 5;
         boolean result = largerThan5.test("hello");
 
         System.out.println(result);
+
+        Consumer<String> printUpperCase = s -> System.out.println(s.toUpperCase());
+        printUpperCase.accept("seven days later");
+
+        doSomething(largerThan5);
+        doSomething(s -> s.startsWith("C"));
+    }
+
+    public static void doSomething(Predicate<String> p){
+        System.out.println(p.test("something"));
     }
 }
