682e5bd1c59c51153f54bcc288ade08098a8a002
Piotr <xmestre@wp.pl>
2023-12-11 18:41:42 +0000

    Advanced Java - Code Challenge

---
+++ src/Standalone_courses/Advanced_Java/Chapter_1_Streams_and_Lambda_Expressions/CodeChallenge.java
@@ -0,0 +1,18 @@
+package Standalone_courses.Advanced_Java.Chapter_1_Streams_and_Lambda_Expressions;
+
+import java.util.List;
+
+public class CodeChallenge {
+
+    // Change these boolean values to control whether you see
+    // the expected result and/or hints.
+    static boolean showExpectedResult = false;
+    static boolean showHints = false;
+
+
+    // Return the highest grade for the students with a specific major
+    static int findAnswer(List<Student> students, String major) {
+        // Your code goes here.
+        return 0;
+    }
+}
