c7f58ae8efdee739de782a57e61ddcfe91a4214a
Piotr <xmestre@wp.pl>
2024-01-12 17:30:32 +0000

    Java Essentials - redoing some basic concepts

---
+++ src/Getting_Started_as_a_Java_Developer/Java_Essential_Training_Syntax_and_Structure/java_essentials/GrossPayCalculator.java
@@ -0,0 +1,21 @@
+package Getting_Started_as_a_Java_Developer.Java_Essential_Training_Syntax_and_Structure.java_essentials;
+
+public class GrossPayCalculator {
+
+    // 2.3. Java Basics - Main method
+    // 02_03 Main method
+
+    public static void main(String[] args) {
+
+        //1. Get the number of hours worked
+        int hours = 40;
+
+        //2. Get the hourly pay rate
+        int payRate;
+
+        //3. Multiply hours and pay rate
+
+        //4. Display results
+
+    }
+}
