cb1fa1a427bb2be85b1a956a04a081de1efb4d34
Piotr <xmestre@wp.pl>
2024-01-21 15:20:05 +0000

    Java Essentials - redoing some basic concepts

--- src/Getting_Started_as_a_Java_Developer/Java_Essential_Training_Syntax_and_Structure/java_essentials/Chapter_4_decision_structures/GradeMessage.java
+++ src/Getting_Started_as_a_Java_Developer/Java_Essential_Training_Syntax_and_Structure/java_essentials/Chapter_4_decision_structures/GradeMessage.java
@@ -9,6 +9,9 @@ import java.util.Scanner;
  */
 public class GradeMessage {
 
+    // Java Essential Training: Syntax and Structure
+    // 04_04 Switch statements
+
     public static void main(String[] args){
 
         System.out.println("Enter your letter grade:");
@@ -17,5 +20,27 @@ public class GradeMessage {
         scanner.close();
 
         String message;
+        switch (grade){
+            case "A":
+                message = "Excellent job!";
+                break;
+            case "B":
+                message = "Great job!";
+                break;
+            case "C":
+                message = "Good job!";
+                break;
+            case "D":
+                message = "You need to work a bit harder";
+                break;
+            case "F":
+                message = "Uh oh!";
+                break;
+            default:
+                message = "Error. Invalid grade";
+                break;
+        }
+
+        System.out.println(grade);
     }
 }
