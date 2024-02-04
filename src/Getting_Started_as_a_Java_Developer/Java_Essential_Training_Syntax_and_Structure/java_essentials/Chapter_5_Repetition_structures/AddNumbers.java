c67b082d9716edfe0081e8f90e34d5724e36302d
Piotr <xmestre@wp.pl>
2024-02-04 21:52:55 +0000

    Java Essentials - redoing some basic concepts

---
+++ src/Getting_Started_as_a_Java_Developer/Java_Essential_Training_Syntax_and_Structure/java_essentials/Chapter_5_Repetition_structures/AddNumbers.java
@@ -0,0 +1,23 @@
+package Getting_Started_as_a_Java_Developer.Java_Essential_Training_Syntax_and_Structure.java_essentials.Chapter_5_Repetition_structures;
+
+import java.util.Scanner;
+
+/*
+ * DO WHILE LOOP:
+ * Write a program that allows a user to enter two numbers,
+ * and then sums up the two numbers. The user should be able to
+ * repeat this action until they indicate they are done.
+ */
+public class AddNumbers {
+
+    // Java Essential Training: Syntax and Structure
+    // 05_02 Do while Loop
+
+    public static void main(String args[]){
+
+        Scanner scanner = new Scanner(System.in);
+
+
+        scanner.close();
+    }
+}
