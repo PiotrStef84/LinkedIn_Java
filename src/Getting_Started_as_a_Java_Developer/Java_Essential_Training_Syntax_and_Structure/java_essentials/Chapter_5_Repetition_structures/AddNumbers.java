f22beea21b0de518671c0e2541c4cc0fd97fec06
Piotr <xmestre@wp.pl>
2024-02-05 22:51:19 +0000

    Java Essentials - redoing some basic concepts

--- src/Getting_Started_as_a_Java_Developer/Java_Essential_Training_Syntax_and_Structure/java_essentials/Chapter_5_Repetition_structures/AddNumbers.java
+++ src/Getting_Started_as_a_Java_Developer/Java_Essential_Training_Syntax_and_Structure/java_essentials/Chapter_5_Repetition_structures/AddNumbers.java
@@ -1,5 +1,7 @@
 package Getting_Started_as_a_Java_Developer.Java_Essential_Training_Syntax_and_Structure.java_essentials.Chapter_5_Repetition_structures;
 
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Scanner;
 
 /*
@@ -16,6 +18,26 @@ public class AddNumbers {
     public static void main(String args[]){
 
         Scanner scanner = new Scanner(System.in);
+
+        int runAgain = 0;
+
+        do{
+            System.out.println("Enter the first number");
+            double number1 = scanner.nextDouble();
+
+            System.out.println("Enter the second number");
+            double number2 = scanner.nextDouble();
+
+            double sum = number1 + number2;
+            System.out.println("The sum is " + sum);
+
+            System.out.println("Would you like to try again");
+            System.out.println("Press: ");
+            System.out.println("1 - Yes");
+            System.out.println("2 - No");
+            runAgain = scanner.nextInt();
+
+        }while (runAgain == 1);
 
 
         scanner.close();
