0d4278866916def9d8706b6bda9787e646245cc0
Piotr <xmestre@wp.pl>
2024-02-06 21:00:38 +0000

    Java Essentials - redoing some basic concepts

---
+++ src/Getting_Started_as_a_Java_Developer/Java_Essential_Training_Syntax_and_Structure/java_essentials/Chapter_5_Repetition_structures/Cashier.java
@@ -0,0 +1,29 @@
+package Getting_Started_as_a_Java_Developer.Java_Essential_Training_Syntax_and_Structure.java_essentials.Chapter_5_Repetition_structures;
+
+/*
+ * FOR LOOP:
+ * Write a cashier program that will scan a given number of items and tally the cost.
+ */
+
+import java.util.Scanner;
+
+public class Cashier {
+
+    // Java Essential Training: Syntax and Structure
+    // 05_03 For loop
+
+    public static void main(String[] args){
+
+        //Get number of items to scan
+        System.out.println("Enter the number of items you would you like to scan:");
+        Scanner scanner = new Scanner(System.in);
+
+        int quantity = scanner.nextInt();
+
+        double total = 0;
+
+        //Create loop to iterate through all of the items and accumulate the costs
+
+
+    }
+}
