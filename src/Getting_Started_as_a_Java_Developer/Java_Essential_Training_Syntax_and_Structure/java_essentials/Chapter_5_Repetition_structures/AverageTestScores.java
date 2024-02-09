4e21bb28eb0fdd5852224d30b1949ebff2c3d424
Piotr <xmestre@wp.pl>
2024-02-09 22:32:47 +0000

    Java Essentials - redoing some basic concepts

---
+++ src/Getting_Started_as_a_Java_Developer/Java_Essential_Training_Syntax_and_Structure/java_essentials/Chapter_5_Repetition_structures/AverageTestScores.java
@@ -0,0 +1,23 @@
+package Getting_Started_as_a_Java_Developer.Java_Essential_Training_Syntax_and_Structure.java_essentials.Chapter_5_Repetition_structures;
+
+import java.util.Scanner;
+
+/*
+ * NESTED LOOPS:
+ * Find the average of each student's test scores
+ */
+public class AverageTestScores {
+
+    public static void main(String[] args){
+
+        //Initialize what we know
+        int numberOfStudents = 24;
+        int numberOfTests = 4;
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+        scanner.close();
+    }
+}
