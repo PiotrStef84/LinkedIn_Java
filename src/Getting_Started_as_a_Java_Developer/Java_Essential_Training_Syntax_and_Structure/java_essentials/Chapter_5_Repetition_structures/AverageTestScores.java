cef8ced1d1b26bd9bbe6f30726589a3d4adc0686
Piotr <xmestre@wp.pl>
2024-02-12 23:16:14 +0000

    Java Essentials - redoing some basic concepts

--- src/Getting_Started_as_a_Java_Developer/Java_Essential_Training_Syntax_and_Structure/java_essentials/Chapter_5_Repetition_structures/AverageTestScores.java
+++ src/Getting_Started_as_a_Java_Developer/Java_Essential_Training_Syntax_and_Structure/java_essentials/Chapter_5_Repetition_structures/AverageTestScores.java
@@ -16,6 +16,20 @@ public class AverageTestScores {
 
         Scanner scanner = new Scanner(System.in);
 
+        //Process all students
+        for(int i=0; i<numberOfStudents; i++){
+
+            double total = 0;
+
+            //Process a student test score
+            for(int j=0; j<numberOfTests; j++){
+
+                System.out.println("Enter test nr " + j+1 +" score");
+                total += scanner.nextInt();
+            }
+
+        }
+
 
 
         scanner.close();
